b87d533c17a76d31f21b530ba9a7b8e9d734666e
Santhosh <santhoosh@vmi481301.contaboserver.net>
2020-12-15 19:42:57 +0530

    m30s: use aosp_base for android-10

--- omni_m30s.mk
+++ omni_m30s.mk
@@ -17,8 +17,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := m30s
 
-
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, build/target/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
