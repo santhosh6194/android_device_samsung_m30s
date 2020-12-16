3cbe346525ade3c79618b368c668ef1734bdbff1
Santhosh <santhoosh@vmi481301.contaboserver.net>
2020-12-16 20:49:22 +0530

    m30s: use embedded for android-9

--- omni_m30s.mk
+++ omni_m30s.mk
@@ -17,7 +17,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := m30s
 
-$(call inherit-product, build/target/product/aosp_base.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
